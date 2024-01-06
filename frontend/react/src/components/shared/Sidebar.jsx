e20e35ac668ea42c0303fa7592cd83e1eafcc92d
AcidCommunism <max.zamota@gmail.com>
2024-01-06 15:03:57 +0400

    Dockerize react ui and rename frontend directory

--- frontend/react/src/components/shared/Sidebar.jsx
+++ frontend/react/src/components/shared/Sidebar.jsx
@@ -33,21 +33,21 @@ import {
 } from 'react-icons/fi';
 
 const LinkItems = [
-    {name: 'Home', icon: FiHome},
-    {name: 'Trending', icon: FiTrendingUp},
-    {name: 'Explore', icon: FiCompass},
-    {name: 'Favourites', icon: FiStar},
-    {name: 'Settings', icon: FiSettings},
+    { name: 'Home', icon: FiHome },
+    { name: 'Trending', icon: FiTrendingUp },
+    { name: 'Explore', icon: FiCompass },
+    { name: 'Favourites', icon: FiStar },
+    { name: 'Settings', icon: FiSettings },
 ];
 
-const SidebarContent = ({onClose, ...rest}) => {
+const SidebarContent = ({ onClose, ...rest }) => {
     return (
         <Box
             transition="3s ease"
             bg={useColorModeValue('white', 'gray.900')}
             borderRight="1px"
             borderRightColor={useColorModeValue('gray.200', 'gray.700')}
-            w={{base: 'full', md: 60}}
+            w={{ base: 'full', md: 60 }}
             pos="fixed"
             h="full"
             {...rest}
@@ -76,7 +76,7 @@ const SidebarContent = ({onClose, ...rest}) => {
                     alt="Spring Sandbox"
                 />
                 <CloseButton
-                    display={{base: 'flex', md: 'none'}}
+                    display={{ base: 'flex', md: 'none' }}
                     onClick={onClose}
                 />
             </Flex>
@@ -89,13 +89,13 @@ const SidebarContent = ({onClose, ...rest}) => {
     );
 };
 
-const NavItem = ({icon, children, ...rest}) => {
+const NavItem = ({ icon, children, ...rest }) => {
     return (
         <Box
             as="a"
             href="#"
-            style={{textDecoration: 'none'}}
-            _focus={{boxShadow: 'none'}}
+            style={{ textDecoration: 'none' }}
+            _focus={{ boxShadow: 'none' }}
         >
             <Flex
                 align="center"
@@ -105,7 +105,7 @@ const NavItem = ({icon, children, ...rest}) => {
                 role="group"
                 cursor="pointer"
                 _hover={{
-                    bg: 'cyan.400',
+                    bg: 'teal.400',
                     color: 'white',
                 }}
                 {...rest}
@@ -126,29 +126,29 @@ const NavItem = ({icon, children, ...rest}) => {
     );
 };
 
-const MobileNav = ({onOpen, ...rest}) => {
+const MobileNav = ({ onOpen, ...rest }) => {
     return (
         <Flex
-            ml={{base: 0, md: 60}}
-            px={{base: 4, md: 4}}
+            ml={{ base: 0, md: 60 }}
+            px={{ base: 4, md: 4 }}
             height="20"
             alignItems="center"
             bg={useColorModeValue('white', 'gray.900')}
             borderBottomWidth="1px"
             borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
-            justifyContent={{base: 'space-between', md: 'flex-end'}}
+            justifyContent={{ base: 'space-between', md: 'flex-end' }}
             {...rest}
         >
             <IconButton
-                display={{base: 'flex', md: 'none'}}
+                display={{ base: 'flex', md: 'none' }}
                 onClick={onOpen}
                 variant="outline"
                 aria-label="open menu"
-                icon={<FiMenu/>}
+                icon={<FiMenu />}
             />
 
             <Text
-                display={{base: 'flex', md: 'none'}}
+                display={{ base: 'flex', md: 'none' }}
                 fontSize="2xl"
                 fontFamily="monospace"
                 fontWeight="bold"
@@ -156,19 +156,19 @@ const MobileNav = ({onOpen, ...rest}) => {
                 Logo
             </Text>
 
-            <HStack spacing={{base: '0', md: '6'}}>
+            <HStack spacing={{ base: '0', md: '6' }}>
                 <IconButton
                     size="lg"
                     variant="ghost"
                     aria-label="open menu"
-                    icon={<FiBell/>}
+                    icon={<FiBell />}
                 />
                 <Flex alignItems={'center'}>
                     <Menu>
                         <MenuButton
                             py={2}
                             transition="all 0.3s"
-                            _focus={{boxShadow: 'none'}}
+                            _focus={{ boxShadow: 'none' }}
                         >
                             <HStack>
                                 <Avatar
@@ -178,7 +178,7 @@ const MobileNav = ({onOpen, ...rest}) => {
                                     }
                                 />
                                 <VStack
-                                    display={{base: 'none', md: 'flex'}}
+                                    display={{ base: 'none', md: 'flex' }}
                                     alignItems="flex-start"
                                     spacing="1px"
                                     ml="2"
@@ -188,8 +188,8 @@ const MobileNav = ({onOpen, ...rest}) => {
                                         Admin
                                     </Text>
                                 </VStack>
-                                <Box display={{base: 'none', md: 'flex'}}>
-                                    <FiChevronDown/>
+                                <Box display={{ base: 'none', md: 'flex' }}>
+                                    <FiChevronDown />
                                 </Box>
                             </HStack>
                         </MenuButton>
@@ -203,7 +203,7 @@ const MobileNav = ({onOpen, ...rest}) => {
                             <MenuItem>Profile</MenuItem>
                             <MenuItem>Settings</MenuItem>
                             <MenuItem>Billing</MenuItem>
-                            <MenuDivider/>
+                            <MenuDivider />
                             <MenuItem>Sign out</MenuItem>
                         </MenuList>
                     </Menu>
@@ -213,14 +213,14 @@ const MobileNav = ({onOpen, ...rest}) => {
     );
 };
 
-const SidebarWithHeader = ({children}) => {
-    const {isOpen, onOpen, onClose} = useDisclosure();
+const SidebarWithHeader = ({ children }) => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
     return (
         <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
             <SidebarContent
                 onClose={() => onClose}
-                display={{base: 'none', md: 'block'}}
+                display={{ base: 'none', md: 'block' }}
             />
             <Drawer
                 isOpen={isOpen}
@@ -231,12 +231,12 @@ const SidebarWithHeader = ({children}) => {
                 size="full"
             >
                 <DrawerContent>
-                    <SidebarContent onClose={onClose}/>
+                    <SidebarContent onClose={onClose} />
                 </DrawerContent>
             </Drawer>
             {/* mobilenav */}
-            <MobileNav onOpen={onOpen}/>
-            <Box ml={{base: 0, md: 60}} p="4">
+            <MobileNav onOpen={onOpen} />
+            <Box ml={{ base: 0, md: 60 }} p="4">
                 {children}
             </Box>
         </Box>
