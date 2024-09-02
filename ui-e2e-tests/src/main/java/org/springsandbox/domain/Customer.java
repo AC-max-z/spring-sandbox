77f4501a8fa12322a32f6d605cac6701597f4889
maxzamota <max.zamota@gmail.com>
2024-09-03 00:47:46 +0400

    add lombok

--- ui-e2e-tests/src/main/java/org/springsandbox/domain/Customer.java
+++ ui-e2e-tests/src/main/java/org/springsandbox/domain/Customer.java
@@ -1,48 +1,15 @@
 package org.springsandbox.domain;
 
-public class Customer {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+public class Customer {
     private String name;
     private String email;
     private int age;
     private String gender;
-
-    public Customer(String name, String email, int age, String gender) {
-        this.name = name;
-        this.email = email;
-        this.age = age;
-        this.gender = gender;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getGender() {
-        return gender;
-    }
 }
