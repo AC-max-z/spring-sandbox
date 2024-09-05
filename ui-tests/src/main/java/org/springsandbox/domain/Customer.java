243a81a547be65e842685527a2edbe567ea5ce2d
maxzamota <max.zamota@gmail.com>
2024-09-05 17:13:59 +0400

    forgot something

--- ui-tests/src/main/java/org/springsandbox/domain/Customer.java
+++ ui-tests/src/main/java/org/springsandbox/domain/Customer.java
@@ -3,10 +3,12 @@ package org.springsandbox.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@ToString
 public class Customer {
     private String name;
     private String email;
