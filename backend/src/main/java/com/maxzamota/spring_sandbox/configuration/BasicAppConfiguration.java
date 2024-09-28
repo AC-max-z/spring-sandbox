5689a472a70ab191768ac25d1e9ad79122b32dc3
maxzamota <max.zamota@gmail.com>
2024-09-29 03:26:49 +0400

    update backend configuration

--- backend/src/main/java/com/maxzamota/spring_sandbox/configuration/BasicAppConfiguration.java
+++ backend/src/main/java/com/maxzamota/spring_sandbox/configuration/BasicAppConfiguration.java
@@ -2,13 +2,31 @@ package com.maxzamota.spring_sandbox.configuration;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.record.RecordModule;
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
+
+import java.util.concurrent.Executors;
 
 @Configuration
 public class BasicAppConfiguration {
     @Bean
     public ModelMapper mapper() {
         return new ModelMapper().registerModule(new RecordModule());
+    }
+
+    @Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
+    public AsyncTaskExecutor asyncTaskExecutor() {
+        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
+    }
+
+    @Bean
+    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+        return protocolHandler -> {
+            protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+        };
     }
 }
