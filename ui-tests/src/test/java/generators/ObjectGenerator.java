adb4c685bb73d6ba6ee24960d8907041eb309168
maxzamota <max.zamota@gmail.com>
2024-10-02 14:52:50 +0400

    bump

--- ui-tests/src/test/java/generators/ObjectGenerator.java
+++ ui-tests/src/test/java/generators/ObjectGenerator.java
@@ -6,9 +6,9 @@ import java.util.List;
 public interface ObjectGenerator<T> {
     T generate();
 
-    default List<T> generateMany(int howMuch) {
-        var list = new ArrayList<T>(howMuch);
-        for (int i = 0; i < howMuch; i++) {
+    default List<T> generateMany(int howMany) {
+        var list = new ArrayList<T>(howMany);
+        for (int i = 0; i < howMany; i++) {
             list.add(this.generate());
         }
         return list.stream().toList();
