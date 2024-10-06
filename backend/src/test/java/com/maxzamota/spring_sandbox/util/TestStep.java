805069b5902fdaa61ca6ee1646e369b0148c89bc
maxzamota <max.zamota@gmail.com>
2024-10-07 00:21:38 +0400

    bump

---
+++ backend/src/test/java/com/maxzamota/spring_sandbox/util/TestStep.java
@@ -0,0 +1,26 @@
+package com.maxzamota.spring_sandbox.util;
+
+import io.qameta.allure.Allure;
+import org.slf4j.Logger;
+
+import java.util.concurrent.Callable;
+
+public class TestStep {
+
+    public static <T> T step(String step, Logger logger, Callable<T> lambda) throws Exception {
+        Allure.step(step);
+        logger.info(step);
+        return lambda.call();
+    }
+
+    public static void step(String step, Logger logger, Runnable action) {
+        Allure.step(step);
+        logger.info(step);
+        action.run();
+    }
+
+    public static void step(String step, Logger logger) {
+        Allure.step(step);
+        logger.info(step);
+    }
+}
